0bc4f49d6512e04f6eee3d4b990770d51bd2914f
Cris_mp <cristina.mgzp@gmail.com>
2025-01-08 21:33:41 +0100

    Inicio de proyecto

---
+++ core/src/main/java/io/crismp/frogGame/FirstScreen.java
@@ -0,0 +1,57 @@
+package io.crismp.frogGame;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/** First screen of the application. Displayed after the application is created. */
+public class FirstScreen implements Screen {
+
+    private Texture fondo;
+    private Sprite fondosprite;
+    private SpriteBatch batch;
+
+
+    @Override
+    public void show() {
+        // Prepare your screen here.
+        fondo = new Texture("cover.png");
+        fondosprite = new Sprite(fondo);
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render(float delta) {
+        // Draw your screen here. "delta" is the time since last render in seconds.
+        batch.begin();
+        fondosprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your screen here. The parameters represent the new window size.
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void hide() {
+        // This method is called when another screen replaces this one.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy screen's assets here.
+        fondosprite.getTexture().dispose();
+    }
+}
